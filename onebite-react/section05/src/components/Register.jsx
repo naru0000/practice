38ca6c353fe6d5751221bffb297ddf5fab0c7d0c
woori ko <mynameiswooriko@gmail.com>
2025-12-01 22:08:12 +0900

    한입강의 chapter6 학습: useRef

--- onebite-react/section05/src/components/Register.jsx
+++ onebite-react/section05/src/components/Register.jsx
@@ -1,4 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+// useRef는 레퍼런스 객체를 생성한다.
+// 컴포넌트 내부의 변수로 활용 가능하다.
+// 어떤 경우에도 리렌더링을 유발하지 않는다.
+// 특정 DOM요소에 접근 가능하다.
 
 const Register = () => {
   const [input, setInput] = useState({
@@ -8,9 +13,19 @@ const Register = () => {
     bio: "",
   });
 
+  const countRef = useRef(0);
+  const inpurRef = useRef();
+
   // 통합 이벤트 핸들러
 
+  // useref를 쓰지 않고 count를 쓰게 되면 register함수가 계속 재렌더링 되기 때문에 카운트는 계속 1이 나옴
+  // let count = 0; count를 함수 외부에 선언하면 되지 않느냐? 라는 질문이 나올 수 있음
+  // 그렇게 하면 동작은 되지만 register 컴포넌트를 부모컴포넌트에서 여러개 썼을때 카운트가 똑같은 변수를 공유하게 됨 ex)register a의 제출 버튼 클릭시 register b의 제출 카운트도 올라간다.
+
   const onChange = (e) => {
+    countRef.current++;
+    // count++;
+    console.log(countRef);
     setInput({
       ...input,
       [e.target.name]: e.target.value, // 대괄호 안 프로퍼티는 객체의 키 값으로 쓰임
@@ -19,10 +34,18 @@ const Register = () => {
     console.log(e.target.name, e.target.value);
   };
 
+  const onSubmit = () => {
+    if (input.name === "") {
+      // 이름을 입력하는 dom에 포커스
+      console.log(inpurRef.current);
+      inpurRef.current.focus();
+    }
+  };
+
   return (
     <div>
       <div>
-        <input name="name" value={input.name} onChange={onChange} type="text" placeholder={"이름"} />
+        <input ref={inpurRef} name="name" value={input.name} onChange={onChange} type="text" placeholder={"이름"} />
         {input.name}
       </div>
       <div>
@@ -42,6 +65,7 @@ const Register = () => {
         <textarea name="bio" value={input.bio} onChange={onChange}></textarea>
         {input.bio}
       </div>
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
 };
